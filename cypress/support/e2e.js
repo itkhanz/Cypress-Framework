d08c82ad0cb6c23e27f138e708c050813af53487
itkhan <itkhanz@gmail.com>
2023-09-05 20:03:33 +0200

    ignore all console errors

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -23,3 +23,12 @@ import 'cypress-mochawesome-reporter/register';
 
 const registerCypressGrep = require('@cypress/grep')
 registerCypressGrep()
+
+// ignore all console errors in app
+//prevents failure of test on console errors while loading website
+// eslint-disable-next-line no-unused-vars
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+})
